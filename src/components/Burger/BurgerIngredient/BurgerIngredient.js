635fb0d872aa27814d877b21a735bd3a09ab6afb
M. Yusuf Ozkan <myozkan@yandex.com>
2020-02-15 16:28:22 +0300

    started the burger component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-import classes from 'BurgerIngredients.module.css';
+import classes from './BurgerIngredient.module.css';
 
 class BurgerIngredient extends Component {
     render() {
@@ -10,14 +10,12 @@ class BurgerIngredient extends Component {
         switch (this.props.type) {
             case ('bread-bottom'):
                 ingredient = <div className={classes.BreadBottom}></div>;
-                break
+                break;
             case ('bread-top'):
-                ingredient = (
-                    <div clasName={classes.BreadTop}>
-                        <div className={classes.Seeds1}></div>
-                        <div className={classes.Seeds2}></div>
-                    </div>
-                );
+                ingredient = <div className={classes.BreadTop}>
+                    <div className={classes.Seeds1}></div>
+                    <div className={classes.Seeds2}></div>
+                </div>;
                 break;
             case ('meat'):
                 ingredient = <div className={classes.Meat}></div>
